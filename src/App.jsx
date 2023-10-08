723df4fcf28eb7de9c7ec230bf1d27bab62e48fc
Fer-nomasss <ferdanielavilacuevas@gmail.com>
2023-10-08 16:08:37 -0300

    prohibido subir

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import Noticias from "./lib/Noticias";
 import Acercade from './lib/Acercade';
 import Proyectos from './lib/Proyectos';
+import Footer from "./lib/Footer";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
       <Route path='/' element={<Header/>} >
         <Route path='noticias' element={<Noticias/>}></Route>
         <Route path='/' element={<Body/>}></Route>
+        <Route path='/' element={<Footer/>}></Route>
         <Route path='acercade' element={<Acercade/>}></Route>
         <Route path='proyectos' element={<Proyectos/>}></Route>
       </Route>
